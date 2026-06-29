1c19999091d3ead2e7c4b528fb8fe1679f61ca62
Nisha Mandiwal <113528385+Nisha8817@users.noreply.github.com>
2026-06-29 15:30:08 +0530

    Refactor EvenOrOdd.java by removing main method
    Removed the main method and its logic for checking even or odd.

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,11 +1,3 @@
 package javacodes;
 import java.util.*;
-public class EvenOrOdd {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a number to check whether it iis even or odd : ");
-		int num = sc.nextInt();
-		if(num%2==0) System.out.println(num+" is Even number");
-		else System.out.println(num+" is Odd number");
-	}
-}
+
